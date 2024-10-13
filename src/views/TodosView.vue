1fb93293bf3f30f6bf96de8867ffa189cb8a69e4
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-13 01:36:27 -0300

    fix: padding of the add task button

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -6,7 +6,7 @@ const { handleAddTodo } = useStoreTodoForm();
     <section class="container px-4 pb-10 mx-auto">
         <div class="py-10 flex items-center justify-between">
             <h1>Todo List</h1>
-            <ElButton type="success" @click="handleAddTodo">Add task</ElButton>
+            <el-button type="success" @click="handleAddTodo" class="max-sm:!px-4">Add task</el-button>
         </div>
         <TodosList />
     </section>
