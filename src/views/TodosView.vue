8afb78ca59e292b839ab132e125109c483e421ae
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-13 22:50:31 -0300

    fix: add main tag to TodosView

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
-import { useStoreTodoForm } from '@/stores/todo-form';
+import { useStoreTodoForm } from "@/stores/todo-form";
 const { handleAddTodo } = useStoreTodoForm();
 </script>
 <template>
+  <main>
     <section class="container px-4 pb-10 mx-auto">
-        <div class="py-10 flex items-center justify-between">
-            <h1>Todo List</h1>
-            <el-button type="success" @click="handleAddTodo" class="max-sm:!px-4">Add task</el-button>
-        </div>
-        <TodosList />
+      <div class="py-10 flex items-center justify-between">
+        <h1>Todo List</h1>
+        <el-button type="success" @click="handleAddTodo" class="max-sm:!px-4"
+          >Add task</el-button
+        >
+      </div>
+      <TodosList />
     </section>
-    <TodoModal />
+  </main>
+  <TodoModal />
 </template>
