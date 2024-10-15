4ef22846d5d39d1826366397b8d8ca457aa27b39
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-10-15 07:27:32 -0300

    fix: home view semantic markup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="prose mx-auto flex flex-wrap px-4 lg:(py-18 px-12) max-w-prose">
-    <div>
-      <h1>To-do list</h1>
-      <p>online to-do list where you can save and organize your tasks.</p>
-    </div>
-    <LoginForm class="!w-full"/>
-  </div>
+  <main>
+    <section
+      class="prose mx-auto flex flex-wrap px-4 lg:(py-18 px-12) max-w-prose"
+    >
+      <hgroup>
+        <h1>To-do list</h1>
+        <p>online to-do list where you can save and organize your tasks.</p>
+      </hgroup>
+      <LoginForm class="!w-full" />
+    </section>
+  </main>
 </template>
